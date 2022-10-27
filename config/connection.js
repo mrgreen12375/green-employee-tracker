f4ebfb7d37829993c909a74ca1b69ba6c74ca4f2
Steven Green <ste2144105@gmail.com>
2022-10-26 22:01:00 -0700

    setup connection

--- config/connection.js
+++ config/connection.js
@@ -1,1 +1,14 @@
 const mysql = require('mysql2');
+
+
+const connection = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'employee_db',
+    },
+    console.log('Connected to DB')
+)
+
+module.exports = connection;
