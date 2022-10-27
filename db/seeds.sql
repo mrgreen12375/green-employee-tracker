9fd699038c77bb55f4aafd9dad5b2c743896cc63
Steven Green <ste2144105@gmail.com>
2022-10-26 22:05:21 -0700

    setup seed data

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,3 +4,18 @@ VALUES
     ("Finance"),
     ("Legal"),
     ("Sales");
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+    ("Lawyer", 190000, 3),
+    ("Salesperson", 60000, 4),
+    ("Software Engineer", 120000, 1),
+    ("Accountant", 125000, 2);
+
+INSERT INTO employee(first_name, last_name)
+VALUES
+    ("Ron", "Weasley"),
+    ("Hermione", "Granger"),
+    ("Draco", "Malfoy"),
+    ("Neville", "Longbottom"),
+    ("Tom", "Riddle");  
